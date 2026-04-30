80786a3d9ae924d31d45013508dbcd9b92efaf95
TMon283 <lemonboy2k6@gmail.com>
2026-04-30 17:28:18 +0700

    import @Repository

--- src/main/java/com/example/btvn_3004/repository/DepartmentRepository.java
+++ src/main/java/com/example/btvn_3004/repository/DepartmentRepository.java
@@ -2,5 +2,7 @@ package com.example.btvn_3004.repository;
 
 import com.example.btvn_3004.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {}
--- src/main/java/com/example/btvn_3004/repository/EmployeeRepository.java
+++ src/main/java/com/example/btvn_3004/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.example.btvn_3004.repository;
 
 import com.example.btvn_3004.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
 
